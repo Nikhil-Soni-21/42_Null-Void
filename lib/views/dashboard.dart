4f6effc25d89d80990fac3abcdb3465931fd96dd
raghav <raghavgupta3383@gmail.com>
2021-07-10 11:21:13 +0530

    raghav 11.20 exercise page made

--- lib/views/dashboard.dart
+++ lib/views/dashboard.dart
@@ -7,6 +7,7 @@ import 'package:rive/rive.dart';
 import 'package:shimmer/shimmer.dart';
 import 'package:tracker_app/repos/quotes_api.dart';
 import 'package:tracker_app/repos/storage_api.dart';
+import 'package:tracker_app/views/exercise.dart';
 import 'package:tracker_app/views/stopwatchPage.dart';
 import 'package:tracker_app/views/yoga.dart';
 
@@ -254,7 +255,13 @@ class _DashboardPageState extends State<DashboardPage> {
                   ),
                 )),
             ElevatedButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              exercise()));
+                },
                 style: buttonStyle,
                 child: Padding(
                   padding: const EdgeInsets.symmetric(
